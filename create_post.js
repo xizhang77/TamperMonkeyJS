ade07c19fe1d7ee02d81c815d1200c816b8205e5
Xi Zhang <xizhang1@cs.stonybrook.edu>
2019-06-21 13:21:15 -0700

    Create create_post.js

---
+++ create_post.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         自动发微博
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  没啥，我就试试…
+// @author       www.github.com/xizhang77
+// @match        https://www.weibo.com/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
